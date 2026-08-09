aa0ae3b2d8ee95c5cdce604da44b1e0ebe81fd0e
sthefanypereira <sthefanypereira521@gmail.com>
2026-08-09 17:55:30 -0300

    feat: adiciona exercicio elementosid

---
+++ javascript/ELEMENTOS ID/02/GetElement02.js
@@ -0,0 +1,13 @@
+// GetElentoById = elemento no html pelo seu id   exemplo:
+// console.log = para ver no console do navegador F12 ok
+
+const c1=document.getElementById("c1")
+const c2=document.getElementById("c2")
+const c3=document.getElementById("c3")
+
+
+console.log(c1)              // elemento
+console.log(c1.id)          // id 
+console.log(c1.innerHTML)  //  html
+
+
