a6a1a6e2612da47487b4d92706bcfdcd20e9b396
Sthefany <sthefanypereira521@gmail.com>
2026-08-08 21:14:55 -0300

    Implement map function to log course details
    Added a map function to iterate over an array of courses and log their names and positions.

---
+++ MAP/map.js
@@ -0,0 +1,8 @@
+const cursos=['html', 'css', 'java', 'sql']
+cursos.map((elemento, indice)=>{
+    console.log("curso: " + elemento + " posicao do curso =" + indice)
+})
+
+
+// map usado para pecorrer colecao.
+// se eu quiser pecorrer uma coleçao inteira  sem interomper usa-se o map
