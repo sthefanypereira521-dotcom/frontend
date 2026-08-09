07dfea2f318dc7f782746bbe7ea21c851bc559d8
sthefanypereira <sthefanypereira521@gmail.com>
2026-08-09 17:11:39 -0300

    featadiciona exercicio class name

---
+++ javascript/ELEMENTOS CLASSE NOME/ByClassName.js
@@ -0,0 +1,7 @@
+const cursosTodos=document.getElementsByClassName("curso")
+const cursosC1=document.getElementsByClassName("c1")
+const cursosC2=document.getElementsByClassName("c2")
+
+console.log(cursosTodos)     // todos os elementos da class
+console.log(cursosC1)       // elementos da class C1
+console.log(cursosC2)      //  elementos da class C2
---
+++ javascript/ELEMENTOS CLASSE NOME/className.js
@@ -0,0 +1,10 @@
+// [...] transformando coleçao em array
+// mudando elementos de class ou alterando 
+
+const cursosTodos=[...document.getElementsByClassName("curso")]
+const cursosC1=[...document.getElementsByClassName("c1")]
+const cursosC2=[...document.getElementsByClassName("c2")]
+
+console.log(cursosTodos)     
+console.log(cursosC1)       
+console.log(cursosC2)      
